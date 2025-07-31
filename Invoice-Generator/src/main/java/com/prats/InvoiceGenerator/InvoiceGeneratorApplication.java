8e4dc5aea7d0bf40dc6e96f0c685da4bfd91bac9
PatelPratik39 <ptl.pratik717@gmail.com>
2025-07-30 23:39:13 -0500

    component strcuture is configured

---
+++ Invoice-Generator/src/main/java/com/prats/InvoiceGenerator/InvoiceGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.prats.InvoiceGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceGeneratorApplication.class, args);
+		System.err.println("✅ Invoice Generator Application Started Successfully !!!!!! 🧾");
+	}
+
+}
