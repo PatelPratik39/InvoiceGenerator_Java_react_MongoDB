712f3d2e7aa8ea233f74ded70a52527d31b8f40d
PatelPratik39 <ptl.pratik717@gmail.com>
2025-07-31 23:38:16 -0500

    landing page UI configured but functionality still pending

--- invoice-generator-client/src/pages/MainPage.jsx
+++ invoice-generator-client/src/pages/MainPage.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { Pencil } from 'lucide-react';
+import {useContext, useState} from 'react'
+import {AppContext} from '../context/AppContext.jsx';
+
 
 const MainPage = () => {
+
+  const [isEditingTitle, setIsEditingTitle] = useState(false);
+
+  const{invoiceTitle, setInvoiceTitle} = useContext(AppContext);
+  
+  const handleTitleChange = (e) => {
+      const newTitle = e.target.value;
+      setInvoiceTitle(newTitle);
+  }
+
+  const handleTitleEdit = () => {
+    setInvoiceTitle(true)
+  }
+
+  const handleTitleBlur = () => {
+    
+  }
+
   return (
     <div>
-      MainPage
+      <div className="container-fluid bg-light min-vh-100 py-4">
+        <div className="container">
+          {/* titlebar */}
+          <div className="bg-white border rounded shadow-sm p-3 mb-4">
+            <div className='d-flex align-items-center'>
+                {isEditingTitle ? (
+                    <input type='text' className='form-control me-2' autoFocus/>
+                ) : (
+                  <>
+                    <h5 className='mb-0 me-2'>{invoiceTitle}</h5>
+                    <button className='btn btn-sm p-0 border-0 bg-transparent'>
+                      <Pencil className='text-primary' size={20}/>
+                    </button>
+                  </>
+                )}
+            </div>
+
+          </div>
+          {/* Invoice Form and template grid */}
+          <div className="row g-4 align-items-stretch">
+            {/* Invoice form */}
+              <div className="col-12 col-lg-6 d-flex">
+                  <div className="bg-white border rounded shadow-sm p-4 w-100">
+                        invoice form
+                  </div>
+              </div>
+            {/* Invoice grid */}
+            <div className="col-12 col-lg-6 d-flex">
+                <div className="bg-white border rounded shadow-sm p-4 w-100">
+                    template grid
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
